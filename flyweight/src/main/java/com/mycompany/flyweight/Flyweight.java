a2947f755a49eba820f2f194b264122487cd7ac3
Isaias Vitor Silva Oliveira <isaiasvitorslva@gmail.com>
2024-09-16 21:00:30 -0300

    first commit

---
+++ flyweight/src/main/java/com/mycompany/flyweight/Flyweight.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.flyweight;
+
+/**
+ *
+ * @author isaias-vitor
+ */
+public class Flyweight {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
